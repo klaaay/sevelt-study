40e1c1c389516cb16ecbdb5a04f2e82a6b20e130
klay <klaaay1998@163.com>
2020-12-05 18:22:09 +0800

    feat: 展示tick函数的正确使用方式

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { tick } from "svelte";
   import Product from "./Product.svelte";
   import Modal from "./Modal.svelte";
 
@@ -21,6 +22,27 @@
   }
 
   let showModal = false;
+
+  let text = "this is some dummy text!";
+
+  function transform(event) {
+    if (event.which !== 9) {
+      return;
+    }
+    event.preventDefault();
+    const selectionStart = event.target.selectionStart;
+    const selectionEnd = event.target.selectionEnd;
+    const value = event.target.value;
+    text =
+      value.slice(0, selectionStart) +
+      value.slice(selectionStart, selectionEnd).toUpperCase() +
+      value.slice(selectionEnd);
+
+    tick().then(() => {
+      event.target.selectionStart = selectionStart;
+      event.target.selectionEnd = selectionEnd;
+    });
+  }
 </script>
 
 {#each products as product}
@@ -42,3 +64,5 @@
       disabled={!closeable}>Cancel</button>
   </Modal>
 {/if}
+
+<textarea rows="3" on:keydown={transform} value={text} />
